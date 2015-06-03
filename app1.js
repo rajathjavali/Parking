0374a31c9c8c286efcb1160771617c520b0a0499
rajathjavali <rajathjavali@gmail.com>
2015-06-03 12:16:28 +0530

    modified

--- app1.js
+++ app1.js
@@ -1,12 +1,43 @@
-function verifyorder() {
+
+var spot;
+var size;
+function initialize(num)
+{
+	size = num;
+	spot = new Array(num);
+	for(i=0;i<size;i++)
+	{
+		spot[i]=null;
+	}	
+}
+
+function park(name) {
 		
-        var order = document.getElementById('value').value;
-        if (order > 0) {
-            alert(order);
-            return true;
-        }
-        else {
-            alert("Sorry, you need to enter a positive integer value, try again");
-            document.getElementById('error').innerHTML = "Sorry, you need to enter a positive integer value, try again";
-        }
+    //var name = document.getElementById('value').value;        
+    var place = 0;
+    while(place <size && spot[place] != null) {place++;}
+
+    if (place<size) {
+        spot[place] = name; 
+        return place;
     }
+    else
+    	return -1;
+
+}
+
+function unpark(name){
+	var i=0
+	var place = 0;
+	while(i<size && spot[i]!=name){
+		i++;
+	}
+	if(i<size){		
+		spot[i] = null;
+		return 1;
+	}
+	else
+		return 0;
+
+	}
+}
